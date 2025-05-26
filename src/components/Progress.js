c4574a5776ae74799053ae6a7b705c1067c92121
Bogdan <iordachescubogdann@gmail.com>
2025-05-26 16:30:47 +0300

    Refactored app to use Context API and removed porp drilling

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,11 +1,9 @@
-function Progress({
-  index,
-  numQuestions,
-  points,
-  maxPossiblePoints,
-  answer,
-  question,
-}) {
+import { useQuiz } from "../contexts/QuizContext";
+
+function Progress() {
+  const { index, numQuestions, points, maxPossiblePoints, answer, questions } =
+    useQuiz();
+  const question = questions[index];
   return (
     <header className="progress">
       <progress max={numQuestions} value={index + Number(answer !== null)} />
@@ -13,7 +11,8 @@ function Progress({
         Question <strong>{index + 1}</strong> / {numQuestions}
       </p>
       <p>
-        Worth ({question.points} {question.points === 1 ? "point" : "points"})
+        Worth ({question?.points ?? 0}{" "}
+        {question?.points === 1 ? "point" : "points"})
       </p>
       <p>
         <strong>{points}</strong> / {maxPossiblePoints}
